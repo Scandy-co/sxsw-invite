02878dbb90014db173dd5317eca48cf95f3f4f7f
Neilson Gamble <neilson@scandy.co>
2020-03-02 18:32:41 -0600

    bringing over new code from glitch

--- hud.js
+++ hud.js
@@ -1,72 +1,115 @@
-// $(document).ready(function() {
-//   $(".button").on("mousedown", function() {
-//     var button = this;
+function bindClick(buttonEl) {
+  const button = buttonEl;
+  button.style.boxShadow = "none";
+  button.style.transform = "scale(1.2)";
 
-//     button.style.boxShadow = "none";
-//     button.style.transform = "scale(1.2)";
+  if (button.className === "button button-menu") {
+    document
+      .querySelector(".menuOptionsShader")
+      .classList.add("reveal-menuOptionsShader");
+    document
+      .querySelector(".button-shareSocial")
+      .classList.add("reveal-shareSocial-button");
+    document
+      .querySelector(".button-shareQR")
+      .classList.add("reveal-shareQR-button");
+    document
+      .querySelector(".button-expInfo")
+      .classList.add("reveal-expInfo-button");
+    document
+      .querySelector(".button-eventInfo")
+      .classList.add("reveal-eventInfo-button");
+    document
+      .querySelector(".menuOptionsShader")
+      .classList.remove("hide-menuOptionsShader");
+    document
+      .querySelector(".button-shareSocial")
+      .classList.remove("hide-shareSocial-button");
+    document
+      .querySelector(".button-shareQR")
+      .classList.remove("hide-shareQR-button");
+    document
+      .querySelector(".button-expInfo")
+      .classList.remove("hide-expInfo-button");
+    document
+      .querySelector(".button-eventInfo")
+      .classList.remove("hide-eventInfo-button");
 
-//     if (this.className === "button button-menu") {
-//       $(".menuOptionsShader").addClass("reveal-menuOptionsShader");
-//       $(".button-shareSocial").addClass("reveal-shareSocial-button");
-//       $(".button-shareQR").addClass("reveal-shareQR-button");
-//       $(".button-expInfo").addClass("reveal-expInfo-button");
-//       $(".button-eventInfo").addClass("reveal-eventInfo-button");
-//       $(".menuOptionsShader").removeClass("hide-menuOptionsShader");
-//       $(".button-shareSocial").removeClass("hide-shareSocial-button");
-//       $(".button-shareQR").removeClass("hide-shareQR-button");
-//       $(".button-expInfo").removeClass("hide-expInfo-button");
-//       $(".button-eventInfo").removeClass("hide-eventInfo-button");
+    setTimeout(function() {
+      document.querySelector(".button-menu").classList.add("hide");
+      document.querySelector(".button-menu-close").classList.remove("hide");
+      document.querySelector(".menuOptionsContainer").style.display = "block";
+    }, 200);
+  }
 
-//       setTimeout(function() {
-//         $(".button-menu").addClass("hide");
-//         $(".button-menu-close").removeClass("hide");
-//       }, 200);
-//     }
+  if (button.className === "button button-menu-close") {
+    document
+      .querySelector(".menuOptionsShader")
+      .classList.remove("reveal-menuOptionsShader");
+    document
+      .querySelector(".button-shareSocial")
+      .classList.remove("reveal-shareSocial-button");
+    document
+      .querySelector(".button-shareQR")
+      .classList.remove("reveal-shareQR-button");
+    document
+      .querySelector(".button-expInfo")
+      .classList.remove("reveal-expInfo-button");
+    document
+      .querySelector(".button-eventInfo")
+      .classList.remove("reveal-eventInfo-button");
+    document
+      .querySelector(".menuOptionsShader")
+      .classList.add("hide-menuOptionsShader");
+    document
+      .querySelector(".button-shareSocial")
+      .classList.add("hide-shareSocial-button");
+    document
+      .querySelector(".button-shareQR")
+      .classList.add("hide-shareQR-button");
+    document
+      .querySelector(".button-expInfo")
+      .classList.add("hide-expInfo-button");
+    document
+      .querySelector(".button-eventInfo")
+      .classList.add("hide-eventInfo-button");
 
-//     if (this.className === "button button-menu-close") {
-//       $(".menuOptionsShader").removeClass("reveal-menuOptionsShader");
-//       $(".button-shareSocial").removeClass("reveal-shareSocial-button");
-//       $(".button-shareQR").removeClass("reveal-shareQR-button");
-//       $(".button-expInfo").removeClass("reveal-expInfo-button");
-//       $(".button-eventInfo").removeClass("reveal-eventInfo-button");
-//       $(".menuOptionsShader").addClass("hide-menuOptionsShader");
-//       $(".button-shareSocial").addClass("hide-shareSocial-button");
-//       $(".button-shareQR").addClass("hide-shareQR-button");
-//       $(".button-expInfo").addClass("hide-expInfo-button");
-//       $(".button-eventInfo").addClass("hide-eventInfo-button");
+    setTimeout(function() {
+      document.querySelector(".button-menu").classList.remove("hide");
+      document.querySelector(".button-menu-close").classList.add("hide");
+      document.querySelector(".menuOptionsContainer").style.display = "none";
+    }, 200);
+  }
 
-//       setTimeout(function() {
-//         $(".button-menu").removeClass("hide");
-//         $(".button-menu-close").addClass("hide");
-//       }, 200);
-//     }
+  setTimeout(function() {
+    button.style.boxShadow = "2px 1px 12px 1px #0c51046b";
+    button.style.transform = "scale(1)";
+  }, 50);
+}
 
-//     setTimeout(function() {
-//       button.style.boxShadow = "2px 1px 12px 1px #0c51046b";
-//       button.style.transform = "scale(1)";
-//     }, 50);
-//   });
-// });
+function bindHudClickEvents() {
+  const buttons = document.querySelectorAll(".button");
+  buttons.forEach(b => b.addEventListener("click", () => bindClick(b)));
+}
 
-// function handleShareClick() {
-//   const shareData = {
-//     text: "Come check out Scandy's 3D art installation!",
-//     url: "https://scandy-sxsw.glitch.me/"
-//   };
-
-//   window.navigator.share(shareData);
-// }
-
-var init = function() {
-  // Handler when the DOM is fully loaded
-  console.log("init!!");
-};
+function hudInit() {
+  bindHudClickEvents();
+}
 
 if (
   document.readyState === "complete" ||
   (document.readyState !== "loading" && !document.documentElement.doScroll)
 ) {
-  init();
+  hudInit();
 } else {
-  document.addEventListener("DOMContentLoaded", callback);
+  document.addEventListener("DOMContentLoaded", hudInit);
+}
+
+function handleShareClick() {
+  const shareData = {
+    text: "Check out Scandy's 3D art installation!",
+    url: "https://atx.scandy.co/"
+  };
+
+  window.navigator.share(shareData);
 }
